e98f3ae1f9fee668fa911a0b0c65aeea721430c1
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-03 16:04:36 +0100

    Handle user auth in coursier

--- homebrew/src/main/ruby/jgows.rb
+++ homebrew/src/main/ruby/jgows.rb
@@ -2,7 +2,23 @@ class Jgows < Formula
   desc "Scala Native Workshop"
   homepage "https://github.com/Tapad/scala-native-workshop"
 
-  url "https://nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/{{ version }}/jgows-{{ version }}.zip"
+  credentials_file = "#{ENV['HOME']}/.ivy2/.credentials"
+  got_ivy_credentials = File.file?(credentials_file)
+  if !got_ivy_credentials
+    abort("Please provide your Nexus credentials in #{credentials_file}")
+  end
+
+  username=File.readlines(credentials_file).map { |line| if line =~ /user=(.+)/i; $1; end }.find{|x|!x.nil?}
+  password=File.readlines(credentials_file).map { |line| if line =~ /password=(.+)/i; $1; end }.find{|x|!x.nil?}
+
+  if username.nil? || password.nil?
+    abort("No credentials found in #{credentials_file}")
+  end
+
+  username_encoded = URI.encode(username)
+  password_encoded = URI.encode(password)
+
+  url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/{{ version }}/jgows-{{ version }}.zip"
   sha256 "{{ checksum }}"
   version "{{ version }}"
 
