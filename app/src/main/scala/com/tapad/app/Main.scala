dabe8ebe28b0a05ab40a6b48f3d62c1a3da7b8fb
Paweł Cejrowski <pcejrowski@gmail.com>
2019-02-18 20:38:10 +0100

    3 - Logging (solution)

--- app/src/main/scala/com/tapad/app/Main.scala
+++ app/src/main/scala/com/tapad/app/Main.scala
@@ -1,10 +1,19 @@
 package com.tapad.app
 
 import com.tapad.common.Greeting
+import slogging._
 
 object Main {
+
+  LoggerConfig.factory = TerminalLoggerFactory()
+
   def main(args: Array[String]): Unit = {
     val conf = new Conf(args)
+
+    if (conf.verbose()) {
+      LoggerConfig.level = LogLevel.DEBUG
+    }
+
     val greet = Greeting.greet(conf.name(), conf.exclamationMark())
     println(greet)
   }
