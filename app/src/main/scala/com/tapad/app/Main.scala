7a459595554efba0afad86070030257e07846348
Paweł Cejrowski <pcejrowski@gmail.com>
2019-02-18 20:34:22 +0100

    2 - Parsing command line arguments (solution)

--- app/src/main/scala/com/tapad/app/Main.scala
+++ app/src/main/scala/com/tapad/app/Main.scala
@@ -3,6 +3,9 @@ package com.tapad.app
 import com.tapad.common.Greeting
 
 object Main {
-  def main(args: Array[String]): Unit =
-    println(Greeting.greet(args.head))
+  def main(args: Array[String]): Unit = {
+    val conf = new Conf(args)
+    val greet = Greeting.greet(conf.name(), conf.exclamationMark())
+    println(greet)
+  }
 }
