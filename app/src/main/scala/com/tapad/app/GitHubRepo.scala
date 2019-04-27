60ec099e5671c499208d2f7ea6f281f050bf2bd7
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-04-27 10:55:34 +0200

    7 - Github API and JSON parsing (solution)

--- app/src/main/scala/com/tapad/app/GitHubRepo.scala
+++ app/src/main/scala/com/tapad/app/GitHubRepo.scala
@@ -5,6 +5,5 @@ import json.{ObjectAccessor, accessor}
 case class GitHubRepo(full_name: String, `private`: Boolean)
 
 object GitHubRepo {
-  // https://github.com/MediaMath/scala-json/blob/master/docs/USAGE.md#case-class-usage
-  implicit val acc = ???
+  implicit val acc = ObjectAccessor.create[GitHubRepo]
 }
--- app/src/main/scala/com/tapad/app/Main.scala
+++ app/src/main/scala/com/tapad/app/Main.scala
@@ -2,6 +2,8 @@ package com.tapad.app
 
 import com.softwaremill.sttp._
 import com.tapad.common.Greeting
+import json.JValue
+import org.ekrich.config.{ConfigFactory, ConfigParseOptions, ConfigSyntax}
 import slogging._
 
 object Main {
@@ -18,19 +20,18 @@ object Main {
     val greet = Greeting.greet(conf.name(), conf.exclamationMark())
     println(greet)
 
+    implicit val backend: SttpBackend[Id, Nothing] = CurlBackend(verbose = conf.debug())
+
     ConfigFile.load() match {
       case Some(config) =>
-        val repos: String = sttp.get(uri"https://api.github.com/user/repos").header(???)
-        val jvalue: List[GitHubRepo] = ???
+        val repos: String = sttp.get(uri"https://api.github.com/user/repos")
+          .header("User-Agent", "awesome")
+          .header("Authorization", s"token ${config.authenticationToken}").send().unsafeBody
+        val jvalue: List[GitHubRepo] = JValue.fromString(repos).toObject[List[GitHubRepo]]
         // https://github.com/MediaMath/scala-json/blob/master/docs/USAGE.md#case-class-usage
         println(jvalue.map(_.full_name))
 
       case None => println(s"Config not found ${ConfigFile.defaultConfigPath}")
     }
-
-    implicit val backend: SttpBackend[Id, Nothing] = CurlBackend(verbose = conf.verbose())
-    val ip = sttp.get(uri"https://api.ipify.org").send().unsafeBody
-
-    println(s"Your IP is: $ip")
   }
 }
